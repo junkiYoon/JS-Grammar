0595a11debae21ca2f4e99b91ba7035eacd6fa36
junkiYoon <67030197+junkiYoon@users.noreply.github.com>
2020-12-28 10:40:54 +0900

    Update 3.1.2.js

--- JS Grammar/3.1.2.js
+++ JS Grammar/3.1.2.js
@@ -1,6 +1,6 @@
 var str = 'test';
 console.log(str);
-console.log(str[0], str[1], str[2], str[3]);  // 자동으로 문자열로 저장도니다.
+console.log(str[0], str[1], str[2], str[3]);  // 자동으로 문자열로 저장된다.
 
 // ! 한 번 정의된 문자열은 변하지 않는다.
 
